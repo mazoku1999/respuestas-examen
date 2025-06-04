e60d19687a22f86b8d44419551a0f27d0d5fb5bc
ROGELIO JOSUE CARATA INCA <118940801+mazoku1999@users.noreply.github.com>
2025-06-04 00:48:41 -0400

    Add quiz app with Firestore ID check

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,258 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_udid/flutter_udid.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Preguntas',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const RootPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<RootPage> createState() => _RootPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _RootPageState extends State<RootPage> {
+  bool _loading = true;
+  bool _registered = false;
+  late String _deviceId;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    await Firebase.initializeApp();
+    final udid = await FlutterUdid.udid;
+    final id = (udid.hashCode.abs() % 100000).toString().padLeft(5, '0');
+    _deviceId = id;
+    final doc = await FirebaseFirestore.instance
+        .collection('devices')
+        .doc(id)
+        .get();
+    if (doc.exists) {
+      setState(() {
+        _registered = true;
+        _loading = false;
+      });
+    } else {
+      setState(() {
+        _registered = false;
+        _loading = false;
+      });
+    }
+  }
+
+  Future<void> _register() async {
+    await FirebaseFirestore.instance
+        .collection('devices')
+        .doc(_deviceId)
+        .set({'registered': true});
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _registered = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    if (_loading) {
+      return const Scaffold(
+        body: Center(child: CircularProgressIndicator()),
+      );
+    }
+    if (!_registered) {
+      return Scaffold(
+        appBar: AppBar(title: const Text('Registro')),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('ID: $_deviceId'),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: _register,
+                child: const Text('Registrar'),
+              ),
+            ],
+          ),
+        ),
+      );
+    }
+    return const HomePage();
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  Future<List<dynamic>> _loadQuestions(BuildContext context, String asset) async {
+    final data = await DefaultAssetBundle.of(context).loadString(asset);
+    return json.decode(data) as List<dynamic>;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      appBar: AppBar(title: const Text('Selecciona Preguntas')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
+          children: [
+            _setButton(context, 'Parte 1', 'assets/preguntas_parte1.json'),
+            const SizedBox(height: 16),
+            _setButton(context, 'Parte 2', 'assets/preguntas_parte2.json'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _setButton(BuildContext context, String title, String asset) {
+    return InkWell(
+      onTap: () async {
+        final questions = await _loadQuestions(context, asset);
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (_) => QuestionPage(title: title, questions: questions),
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(24),
+        decoration: BoxDecoration(
+          color: Colors.green.shade100,
+          borderRadius: BorderRadius.circular(12),
+          border: Border.all(color: Colors.green, width: 2),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(title, style: const TextStyle(fontSize: 18)),
+            const Icon(Icons.arrow_forward_ios, size: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class QuestionPage extends StatefulWidget {
+  final String title;
+  final List<dynamic> questions;
+
+  const QuestionPage({super.key, required this.title, required this.questions});
+
+  @override
+  State<QuestionPage> createState() => _QuestionPageState();
+}
+
+class _QuestionPageState extends State<QuestionPage> {
+  int _index = 0;
+  bool? _isCorrect;
+
+  Map<String, dynamic> get _current =>
+      widget.questions[_index] as Map<String, dynamic>;
+
+  void _check(Map<String, dynamic> option) {
+    setState(() {
+      _isCorrect = option['correct'] as bool;
+    });
+  }
+
+  void _next() {
+    if (_index < widget.questions.length - 1) {
+      setState(() {
+        _index++;
+        _isCorrect = null;
+      });
+    } else {
+      Navigator.of(context).pop();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final opciones = (_current['opciones'] as List<dynamic>)
+        .cast<Map<String, dynamic>>();
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              _current['pregunta'] as String,
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            ...opciones.map((o) => _optionTile(o)),
+            const Spacer(),
+            if (_isCorrect != null)
+              Text(
+                _isCorrect! ? 'Correcto!' : 'Incorrecto',
+                style: TextStyle(
+                  color: _isCorrect! ? Colors.green : Colors.red,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: _next,
+              child: Text(_index < widget.questions.length - 1
+                  ? 'Siguiente'
+                  : 'Finalizar'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  Widget _optionTile(Map<String, dynamic> o) {
+    return GestureDetector(
+      onTap: () => _check(o),
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          border: Border.all(color: Colors.grey.shade400),
+        ),
+        child: Row(
+          children: [
+            Checkbox(
+              value: _isCorrect != null && o['correct'] == _isCorrect,
+              onChanged: (_) => _check(o),
+            ),
+            Expanded(child: Text(o['texto'] as String)),
+          ],
+        ),
+      ),
     );
   }
 }
